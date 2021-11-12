47616376d839adc216f08bdd5191802092108549
synodical <lena0323park@naver.com>
2021-11-12 20:28:53 +0900

    Initial Commit

---
+++ BOJ Source Code/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BOJ Source Code
+//
+//  Created by LeeYeEun on 2021/11/12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
